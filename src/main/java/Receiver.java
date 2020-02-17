3e1d3a173836e352014768dd2ca6012c006d68a3
abrah <abraham.berhe@gmail.com>
2020-02-17 15:15:44 -0500

    Point to Active MQ in AWS ec2 instance , unmarshal xml messages to Java Objects using JAXB

--- src/main/java/Receiver.java
+++ src/main/java/Receiver.java
@@ -1,4 +1,7 @@
 
+import java.io.Reader;
+import java.io.StringReader;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -7,9 +10,15 @@ import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
-
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.FactoryConfigurationError;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.log4j.BasicConfigurator;
+
+import com.activemq.sender.model.Student;
+import com.activemq.sender.model.Students;
 /**
  * The first way of implementing Active MQ Listener
  * @author abrah
@@ -29,15 +38,44 @@ public class Receiver {
 	public void receiveMessage() {
 		try {
 			//here we are using a loop back IP, if the broker is on another machine, you need to put that machine's IP
-			factory = new ActiveMQConnectionFactory("tcp://0.0.0.0:61616");
+			factory = new ActiveMQConnectionFactory("tcp://ec2-18-216-2-76.us-east-2.compute.amazonaws.com:61616");
 			connection = factory.createConnection();
 			connection.start();
 			System.out.println("connetion started...");
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			destination = session.createQueue("JCG_QUEUE");
 			consumer = session.createConsumer(destination);
-
-			while (true) {
+			
+			TextMessage tm=null;
+			Message msg = consumer.receive(3000);
+			System.out.println("Listening...");
+			if (msg instanceof TextMessage) {
+				 tm = (TextMessage) msg;
+				System.out.println(tm.getText());
+			} else {
+				System.out.println("Queue Empty");
+				connection.stop();
+				
+			}
+		
+			//create JAXBContext for Students class
+			JAXBContext context=JAXBContext.newInstance(Students.class);				
+			Unmarshaller studentsUnmarshaller=context.createUnmarshaller();
+			
+			//store the message in a Reader and pass the reader to unmarshal method so that it can be read and converted to java object
+			Reader stringReader=new StringReader(tm.getText());
+			
+			//we could also create XMLStreamReader and store the message(tm.getText()) and pass the XMLStreamReader in to the unmarshal method		
+			//XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(tm.getText()));
+			
+			Students students=(Students) studentsUnmarshaller.unmarshal(stringReader);
+			
+			System.out.println(students.getStudentGroupName());
+			for(Student stu:students.getStudentList()) {
+				System.out.println(stu.toString());
+			
+			}
+			/*while (true) {
 				Message msg = consumer.receive(3000);
 				System.out.println("Listening...");
 				if (msg instanceof TextMessage) {
@@ -48,8 +86,8 @@ public class Receiver {
 					connection.stop();
 					break;
 				}
-			}
-		} catch (JMSException e) {
+			}*/
+		} catch (JMSException | JAXBException | FactoryConfigurationError e) {
 			e.printStackTrace();
 		}
 
